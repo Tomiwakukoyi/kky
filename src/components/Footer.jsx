295a30ea010118160c2ff10903f0a73e91e5f2a1
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-05-01 15:37:20 +0100

    .

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="px-9 text-sm flex items-center justify-between mt-5 pb-1">
+      <span>©TOMIWA KUKOYI 2024</span>
+      <span>All Rights Reserved</span>
+    </footer>
+  );
+};
+
+export default Footer;
